8cec566b9647b3103bb43910047869e36a632587
KlumpenM <ruben300799@gmail.com>
2025-05-22 09:49:19 +0200

    Changed form neovim to vscode

--- init.lua
+++
@@ -1,8 +0,0 @@
--- Require the config for remapping keys
-require('config.remap')
--- Require the settings for customizing neovim
-require('config.set')
-
--- Require the setting up for lazy init, so that we can have a folder for all the plugins that we
--- are using
-require('config.lazy_init')
